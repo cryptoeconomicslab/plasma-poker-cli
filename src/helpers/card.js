b6a62ac119c4b9b25b9e4bb7870cc81e4edef16a
Sg <shogo1104@gmail.com>
2018-11-20 16:12:55 +0900

    duplicationLog update

--- src/helpers/card.js
+++ src/helpers/card.js
@@ -4,6 +4,15 @@ const chalk = require('chalk');
 const { CardGroup, OddsCalculator } = require('poker-odds-calculator')
 
 
+mod.idToAbst = function(id){
+  let suits = ['♠','♥','♦','♣']
+  let nums = ['K','A','2','3','4','5','6','7','8','9','10','J','Q']
+  let sIndex = Math.floor((id-1)/13)
+  let nIndex = id%13
+  var s = suits[sIndex]
+  var n = nums[nIndex]
+  return {s:s,n:n}
+}
 mod.idToCard = function(id){
   var {s,n} = mod.idToAbst(id)
   n = n.padStart(2, " ")
@@ -18,48 +27,22 @@ mod.idToCard = function(id){
 
   return `${s}${n}`
 }
-
-mod.idToAbst = function(id){
-  let suits = ['♠','♥','♦','♣']
-  let nums = ['K','A','2','3','4','5','6','7','8','9','10','J','Q']
+mod.idToAsm = function(id){
+  let suits = ['s','h','d','c']
+  let nums = ['K','A','2','3','4','5','6','7','8','9','T','J','Q']
   let sIndex = Math.floor((id-1)/13)
   let nIndex = id%13
   var s = suits[sIndex]
   var n = nums[nIndex]
-  return {s:s,n:n}
-}
-
-mod.abstToAsm = function(abst){
-  var {s,n} = abst
-
-  switch(s){
-    case '♠':
-      s = 's'
-    break;
-    case '♥':
-      s = 'h'
-    break;
-    case '♦':
-      s = 'd'
-    break;
-    case '♣':
-      s = 'c'
-    break;
-  }
-  if(n==10){
-    n = 'T'
-  }
-
   return `${n}${s}`
-
 }
 
 mod.calc = function(idsA, idsB){
   const blk = 10000
   const blkHex = ((blk).toString(16)+"").padStart(32, "0")
 
-  const player1Cards = CardGroup.fromString(idsA.map(i=> mod.abstToAsm(mod.idToAbst(i))+blkHex ).join(" "));
-  const player2Cards = CardGroup.fromString(idsB.map(i=> mod.abstToAsm(mod.idToAbst(i))+blkHex ).join(" "));
+  const player1Cards = CardGroup.fromString(idsA.map(i=> mod.idToAsm(i)+blkHex ).join(" "));
+  const player2Cards = CardGroup.fromString(idsB.map(i=> mod.idToAsm(i)+blkHex ).join(" "));
   const result = OddsCalculator.calculate([player1Cards, player2Cards], []);
   let aRank = result.handranks[0]
   let bRank = result.handranks[1]
@@ -73,6 +56,15 @@ mod.calc = function(idsA, idsB){
   } else {
     throw new Error("compareTo() func is broken")
   }
+  result.duplicationLog.records.map(r=>{
+    let who = r.who == 0 ? chalk.red("Alice") : chalk.blue("Bob") 
+    let src = mod.idToCard(r.srcCard.toId())
+    let dest = mod.idToCard(r.destCard.toId())
+    let newHands = r.newHands.map(c=> mod.idToCard(c.toId()) ).join(" ")
+    console.log("[Duplication]")
+    console.log(who)
+    console.log(`${newHands} (diff: ${src} => ${dest})`)
+  })
   console.log(`Alice: ${aRank.toString()} - rank: ${aRank.getRank()}`);
   console.log(`Bob: ${bRank.toString()} - rank: ${bRank.getRank()}`);
   console.log(`winner: ${winner}`)
@@ -88,36 +80,36 @@ mod.randomId = function(){
   return (r==0) ? mod.randomId() : r
 }
 
-mod.visualize = async function(){
-  const mergeImg = require("merge-img")
-  let img = await mergeImg([
-    imgpath("1F0A3"),
-    imgpath("1F0AE"),
-    imgpath("1F0A1"),
-    imgpath("1F0D9"),
-    imgpath("1F0C3")
-  ],
-    {direction:true, offset: 50}
-  )
-  img.write(HANDS_PATH, _=>{
-    renderRealCard(HANDS_PATH)
-  })
-}
+// mod.visualize = async function(){
+//   const mergeImg = require("merge-img")
+//   let img = await mergeImg([
+//     imgpath("1F0A3"),
+//     imgpath("1F0AE"),
+//     imgpath("1F0A1"),
+//     imgpath("1F0D9"),
+//     imgpath("1F0C3")
+//   ],
+//     {direction:true, offset: 50}
+//   )
+//   img.write(HANDS_PATH, _=>{
+//     renderRealCard(HANDS_PATH)
+//   })
+// }
 
-mod.imgpath = function(name){
-  let mainFilePathArray = require.main.filename.split("/")
-  mainFilePathArray.pop()
-  let rootdir = mainFilePathArray.join("/")
-  return `${rootdir}/assets/cards/${name}.png` 
-}
+// mod.imgpath = function(name){
+//   let mainFilePathArray = require.main.filename.split("/")
+//   mainFilePathArray.pop()
+//   let rootdir = mainFilePathArray.join("/")
+//   return `${rootdir}/assets/cards/${name}.png` 
+// }
   
- mod.renderRealCard = function(absPath){
-  var fs = require('fs');
-  var pictureTube = require('picture-tube')
-  var tube = pictureTube();
-  tube.pipe(process.stdout);
-  fs.createReadStream(absPath).pipe(tube);
-}
+//  mod.renderRealCard = function(absPath){
+//   var fs = require('fs');
+//   var pictureTube = require('picture-tube')
+//   var tube = pictureTube();
+//   tube.pipe(process.stdout);
+//   fs.createReadStream(absPath).pipe(tube);
+// }
   
 
 
